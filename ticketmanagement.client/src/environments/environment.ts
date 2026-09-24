91fe54f4a108768c3fb5ff262a44f3fb00a478e0
kumarpintubkp1997@gmail.com <you@example.com>
2026-09-24 21:33:33 +0530

    enviroment url changed with production

--- ticketmanagement.client/src/environments/environment.ts
+++ ticketmanagement.client/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiBaseUrl: 'https://onlineeducationapp-api-pk-gxdahjc0h8g2gwhc.westus3-01.azurewebsites.net',
-  apiBaseUrl: 'https://localhost:59396' // adjust to your server URL
+  apiBaseUrl: 'https://onlineeducationapp-api-pk-gxdahjc0h8g2gwhc.westus3-01.azurewebsites.net',
+  apiBaseUrl1: 'https://localhost:59396' // adjust to your server URL
 };
 
 /*
